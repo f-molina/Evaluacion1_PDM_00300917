6cceb7a8a4d2c9ab32efc20705b3e412bf2d4c70
f-molina <00300917@uca.edu.sv>
2019-03-29 11:18:47 -0600

    end

--- app/src/main/java/com/example/evaluacion1_pdm/SecondActivity.java
+++ app/src/main/java/com/example/evaluacion1_pdm/SecondActivity.java
@@ -3,6 +3,7 @@ package com.example.evaluacion1_pdm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.evaluacion1_pdm.utils.AppConstant;
@@ -11,6 +12,7 @@ public class SecondActivity extends AppCompatActivity {
 
     private TextView sUser, sEmail;
     private TextView one, two, three, four, five, six, seven, eight, nine, total;
+    private Button mShare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +31,7 @@ public class SecondActivity extends AppCompatActivity {
         eight = findViewById(R.id.eight);
         nine = findViewById(R.id.nine);
         total = findViewById(R.id.second_total);
-
-
+        mShare = findViewById(R.id.share);
 
         Intent mIntent = getIntent();
         if (mIntent != null) {
@@ -47,5 +48,20 @@ public class SecondActivity extends AppCompatActivity {
             nine.setText(mIntent.getStringExtra(AppConstant.TEXT_NINE));
             total.setText(mIntent.getStringExtra(AppConstant.RES));
         }
+
+        mShare.setOnClickListener(v->{
+            String text = sUser.getText().toString();
+            String text1 = sEmail.getText().toString();
+            String text2 = total.getText().toString();
+            Intent shareIntent = new Intent();
+            shareIntent.setType("text/plain");
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.putExtra(AppConstant.TEXT_USER, text);
+            shareIntent.putExtra(AppConstant.TEXT_EMAIL, text1);
+            shareIntent.putExtra(AppConstant.RES, text2);
+            //Sharing to other apps as text
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Username: " + text + "\nEmail: " + text1 + "\nTotal: " + text2);
+            startActivity(shareIntent);
+        });
     }
 }
